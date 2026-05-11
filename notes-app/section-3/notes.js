551ecf08691f9b091cfb53eaea1c5c2ae503e916
Dev Patel <devapatel09092006@gmail.com>
2026-05-11 21:03:32 +0530

    Create notes.js

---
+++ notes-app/section-3/notes.js
@@ -0,0 +1,14 @@
+//
+// Challange 2 = Define and use a function in a new file
+//
+// 1. Create a new file called notes.js
+// 2. Creates getNotes function that returns "Your Notes..."
+// 3. Export getNotes function
+// 4. From app.js, load in and call the function printing message to console
+
+
+const getNotes = function(){
+    return "Your Notes...";
+}
+
+export default getNotes;
