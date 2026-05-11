7a447d423215ff50d41a03bae22d371c4b47a24c
Dev Patel <devapatel09092006@gmail.com>
2026-05-11 21:03:18 +0530

    Create app.js

---
+++ notes-app/section-3/app.js
@@ -0,0 +1,20 @@
+import add from "./utils.js";
+
+// Chgallenge 2
+
+import display from "./notes.js";
+console.log(display());
+
+// Challenge 2 done!
+
+const sum = add(4,2);
+
+console.log(sum);
+
+
+// Lecture - 11
+
+import validator from "validator";
+
+console.log(validator.isEmail("exe.com"));
+console.log(validator.isURL("https://www.youtube.com/"));
