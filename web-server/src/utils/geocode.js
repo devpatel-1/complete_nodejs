051332d8c3004c8bf508759ce2843370f0125a79
Dev Patel <devapatel09092006@gmail.com>
2026-06-16 21:22:19 +0530

    Create geocode.js

---
+++ web-server/src/utils/geocode.js
@@ -0,0 +1,34 @@
+const request = require("request")
+
+const apiKey1 = process.env.MAPBOX_API_KEY
+
+const geocode = (address, callback) => {
+
+    const url = `https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(address)}&access_token=${apiKey1}&limit=1`
+
+    request({ url, json: true }, (error, { body } = {}) => {
+
+        if (error) {
+
+            callback('Unable to connect!', undefined)
+
+        } else if (body.message) {
+
+            callback(body.message, undefined)
+
+        } else if (!body.features || body.features.length === 0) {
+
+            callback('Unable to find location!', undefined)
+
+        } else {
+
+            callback(undefined, {
+                latitude: body.features[0].geometry.coordinates[1],
+                longitude: body.features[0].geometry.coordinates[0],
+                location: body.features[0].properties.full_address
+            })
+        }
+    })
+}
+
+module.exports = geocode
